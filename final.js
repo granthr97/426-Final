ace3fd058bfdaf5bac0e43a986acfd8565a4d2ed
Kirk Swilley <kirkswilley@gmail.com>
2018-12-01 17:16:50 -0500

    Update final.js

--- final.js
+++ final.js
@@ -1,4 +1,4 @@
-var root_url = "http://comp426.cs.unc.edu:3001/api/";
+var root_url = "http://comp426.cs.unc.edu:3001/";
 
 $(document).ready(() => {
     $('#login_btn').on('click', () => {
@@ -24,12 +24,13 @@ $(document).ready(() => {
 
 var construct_page = function(){
 	let body = $('body');
+	body.empty();
 
-	body.empy();
-
-	body.append('button class="weatherAPI" id="weatherAPI">Weather</button>');
+	body.append('<button class="weatherAPI" id="weatherAPI">Weather</button>');
 
 	let formContainer = $('<div class = "formContainer"></div>');
 
 	body.append(formContainer);
+	body.append('<form id="rendered-form"><div class="rendered-form"><div class="fb-text form-group field-text-1543700248535"><label for="text-1543700248535" class="fb-text-label">Text Field</label><input type="text" class="form-control" name="text-1543700248535" id="text-1543700248535"></div><div class="fb-button form-group field-button-1543700258344"><button type="button" name="button-1543700258344" id="button-1543700258344">Button</button></div><div class="fb-text form-group field-text-1543700261387"><label for="text-1543700261387" class="fb-text-label">Text Field</label><input type="text" class="form-control" name="text-1543700261387" id="text-1543700261387"></div></div></form>')
+
 }

b1bf6fbf231027195f4056b12c970f97460ffdc8
kirkswilley <43912435+kirkswilley@users.noreply.github.com>
2018-12-01 16:06:24 -0500

    Update final.js

--- final.js
+++ final.js
@@ -1,1 +1,32 @@
-final.js
+var root_url = "http://comp426.cs.unc.edu:3001/";
+
+$(document).ready(() => {
+    $('#login_btn').on('click', () => {
+	
+	let user = $('#login_user').val();
+	let pass = $('#login_pass').val();
+
+	console.log(user);
+	console.log(pass);
+	
+	$.ajax(root_url + 'login',
+	       {
+		   type: 'GET',
+		   xhrFields: {withCredentials: true},
+		   data: {
+		       username: user,
+		       password: pass
+		   },
+		   success: (response) => {
+		       if (response.status) {
+			   build_question_interface();
+		       } else {
+			   $('#mesg_div').html("Login failed. Try again.");
+		       }
+		   },
+		   error: () => {
+		       alert('error');
+		   }
+	       });
+    });
+});

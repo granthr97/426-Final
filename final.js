173424224a86640afe66eb33b4d60de50f2a3445
Grant Royster <grant@theroysters.com>
2018-12-01 16:43:59 -0500

    Update final.js

--- final.js
+++ final.js
@@ -2,36 +2,25 @@ var root_url = "http://comp426.cs.unc.edu:3001/";
 
 $(document).ready(() => {
     $('#login_btn').on('click', () => {
-	
-	let user = $('#login_user').val();
-	let pass = $('#login_pass').val();
+		let user = $('#login_user').val();
+		let pass = $('#login_pass').val();
 
-	console.log(user);
-	console.log(pass);
-	
-	$.ajax(root_url + 'login',
-	       {
-		   type: 'GET',
-		   xhrFields: {withCredentials: true},
-		   data: {
-		       username: user,
-		       password: pass
-		   },
-		   success: (response) => {
-		       if (response.status) {
-			   buildalert();
-		       } else {
-			   $('#mesg_div').html("Login failed. Try again.");
-		       }
-		   },
-		   error: () => {
-		       alert('error');
-		   }
-	       });
-    });
+		console.log(user);
+		console.log(pass);
+		
+		$.ajax(root_url + 'sessions', {
+			type: 'POST',
+			xhrFields: {withCredentials: true},
+			data: {"user": {
+				    "username": user,
+				    "password": pass
+				  }
+			},
+			success: (response) => {
+				alert('good');
+			}, error: () => {
+				alert('error');
+			}
+		});
+	});
 });
-//hello
-
-function buildalert(){
-	alert("hello");
-}

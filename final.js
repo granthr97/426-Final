60939150eb09f99b6bf07b5ed462582a78e702cb
Grant Royster <grant@theroysters.com>
2018-12-01 21:32:16 -0500

    Update final.js

--- final.js
+++ final.js
@@ -37,166 +37,62 @@ $(document).ready(() => {
 var construct_page = function(){
 	let body = $('body');
 	body.empty();
-	var tickets = {};
-	get_info(tickets, 'ticket', 'tickets', true);
+	var tickets;
+	get_all_info(tickets, 'tickets', true);
 }
 
-// e.g. get_info('itinerary', 'itineraries', false);
-var get_info = function(storage, singular, plural, retrieve_connected_info){
-	$.ajax(root_url + plural, {
+// e.g. get_all_info(array, 'tickets', true, 123);
+var get_all_info = function(storage, name, retrieve_connected_info, id){
+	let url = root_url + name;
+	$.ajax(url, {
 		type: 'GET',
 		xhrFields: {withCredentials: true},
 		success: (response) => {
 			if (!retrieve_connected_info){
 				storage = response;
 			} else {
+				storage = {};
 				for(let i in response){
-					let unit = {
-						"ticket": null,
-						"instance": null,
-						"flight": null,
-						"departure_airport": null,
-						"arrival_airport": null,
-						"itinerary": null,
-						"seat": null,
-						"plane": null,
-						"airline": null,
+					let unit = storage[i];
+					unit = [];
+					unit[name] = response[i];
+					// get_instance(unit, response[i].instance_id);
+					// get_seat(unit, response[i].seat_id);
+					// get_itinerary(unit, response[i].itinerary_id);
+					for (let key in response[i]){
+						if (key.endsWith('_id') && !key.startsWith('user')){
+							let next = key.substring(0, key.length - 3);
+							let id = response[i][key];
+							get_specific_info(unit, next, id);
+						}
 					}
-					storage[i] = unit;
-					unit[singular] = response[i];
-					get_instance(unit, response[i].instance_id);
-					get_seat(unit, response[i].seat_id);
-					get_itinerary(unit, response[i].itinerary_id);
 				}
 			}
-			console.log(storage);
 		}, error: () => {
 			alert('bad');
 		}
 	});
 }
 
-var get_ticket = function(unit, id){
-	if (!id || unit.ticket) { return; }
-	$.ajax(root_url + 'tickets/' + id,{
+var get_specific_info = function(unit, name, id){
+	if (!id || unit[name]) { return; }
+	let url = root_url;
+	if (name == 'itinerary') { url += 'itineraries/' + id;
+	} else if (name == 'departure' || name == 'arrival') {url += 'airports/' + id }
+	else { url += name + 's/' + id; }
+	$.ajax(url,{
 		type: 'GET',
 		xhrFields: {withCredentials: true},
 		success: (response) => {
-			unit.ticket = response;
-			get_itinerary(unit, response.itinerary_id);
-			get_instance(unit, response.instance_id);
-			get_seat(unit, response.seat_id);
-		}, error: () => {
-			alert('bad');
-		}
-	});
-}
-
-var get_airline = function(unit, id){
-	if (!id || unit.airline) { return; }
-	$.ajax(root_url + 'airlines/' + id,{
-		type: 'GET',
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			unit["airline"] = response;
-		}, error: () => {
-			alert('bad');
-		}
-	});
-}
-
-var get_plane = function(unit, id){
-	if (!id || unit.plane) { return; }
-	$.ajax(root_url + 'planes/' + id,{
-		type: 'GET',
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			unit["plane"] = response;
-			get_airline(unit, response.airline_id);
-		}, error: () => {
-			alert('bad');
-		}
-	});
-}
-
-var get_seat = function(unit, id){
-	if (!id || unit.seat) { return; }
-	$.ajax(root_url + 'seats/' + id,{
-		type: 'GET',
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			unit["seat"] = response;
-			get_plane(unit, response.plane_id);
-		}, error: () => {
-			alert('bad');
-		}
-	});
-}
-
-var get_itinerary = function(unit, id){
-	if (!id || unit.itinerary) { return; }
-	$.ajax(root_url + 'itineraries/' + id,{
-		type: 'GET',
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			unit["itinerary"] = response;
-		}, error: () => {
-			alert('bad');
-		}
-	});
-}
-
-var get_instance = function(unit, id){
-	if (!id || unit.instance) { return; }
-	$.ajax(root_url + 'instances/' + id,{
-		type: 'GET',
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			unit["instance"] = response;
-			get_flight(unit, response.flight_id);
-		}, error: () => {
-			alert('bad');
-		}
-	});
-}
-
-var get_flight = function(unit, id){
-	if (!id || unit.flight) { return; }
-	$.ajax(root_url + 'flights/' + id,{
-		type: 'GET',
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			unit["flight"] = response;
-			get_departure_airport(unit, response.departure_id);
-			get_arrival_airport(unit, response.arrival_id);
-			get_plane(unit, response.plane_id);
-			get_airline(unit, response.airline_id);
-		}, error: () => {
-			alert('bad');
-		}
-	});
-}
-
-var get_departure_airport = function(unit, id){
-	if (!id || unit.departure_airport) { return; }
-	$.ajax(root_url + 'airports/' + id,{
-		type: 'GET',
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			unit["departure_airport"] = response;
-		}, error: () => {
-			alert('bad');
-		}
-	});
-}
-
-var get_arrival_airport = function(unit, id){
-	if (!id || unit.arrival_airport) { return; }
-	$.ajax(root_url + 'airports/' + id,{
-		type: 'GET',
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			unit["arrival_airport"] = response;
+			unit[name] = response;
+			for (let key in response){
+				if (key.endsWith('_id') && !key.startsWith('user')){
+					let next = key.substring(0, key.length - 3);
+					let newid = response[key];
+					get_specific_info(unit, next, newid);
+				}
+			}
+			console.log(unit);
 		}, error: () => {
 			alert('bad');
 		}
